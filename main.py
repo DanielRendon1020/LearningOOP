318592406660aeec597f39a208e2336a23c24373
DanielRendon1020 <117864127+DanielRendon1020@users.noreply.github.com>
2023-08-05 17:21:00 +0000

    Race Class

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+class Race():
+  def __init__(self, name, driver_limit, drivers):
+    self.name = name
+    self.driver_limit = driver_limit
+    self.drivers = drivers
+    print(f'Race Info:\n\tRace Name: {self.name}\n\tDriver Limit: {self.driver_limit}\n\tDrivers: {self.drivers}')
+
 class Car:
   def __init__(self, name, max_speed):
     self.name = name
@@ -13,3 +20,5 @@ myCar = Car('Kitt', 180)
 myOtherCar = Car('Speedy', 55)
 
 myCar.talk('Michael')
+
+race = Race("Indy 500", 3, ["Bill", "Jim", "John"])
